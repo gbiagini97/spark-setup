37434335761868819b165bbc70f57daccc9ab1d2
gbiagini97 <gbiagini97@gmail.com>
2019-09-30 12:42:42 +0200

    edit

--- initscript.sh
+++ initscript.sh
@@ -2,9 +2,12 @@
 
 apt update
 apt install wget 
-mkdir /home/sibylla
-export SIBYLLA_HOME=/home/sibylla
-cd /home/sibylla
+cd /home
+
+mkdir sibylla
+export SIBYLLA_HOME="/home/sibylla"
+
+mkdir java
 wget https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
-tar -xzvf openjdk-11+28_linux-x64_bin.tar.gz
-export JAVA_HOME=jdk-11/
+tar -xzvf openjdk-11+28_linux-x64_bin.tar.gz -C java/
+export JAVA_HOME="/home/java/jdk-11"
--- script.sh
+++ script.sh
@@ -3,6 +3,9 @@
 mkdir -p $SIBYLLA_HOME/3d-party/spark-domain/spark-jdk
 mkdir -p $SIBYLLA_HOME/3d-party/spark-domain/spark-home
 
+#to be removed
+mkdir $SIBYLLA_HOME/downloads
+
 #download and uncompress jdk8
 wget -P $SIBYLLA_HOME/downloads/ https://download.java.net/openjdk/jdk8u40/ri/openjdk_jre_ri-8u40-b25-linux-i586-10_feb_2015.tar.gz
 tar -xzvf $SIBYLLA_HOME/downloads/openjdk_jre_ri-8u40-b25-linux-i586-10_feb_2015.tar.gz -C $SIBYLLA_HOME/3d-party/spark-domain/spark-jdk
@@ -13,9 +16,10 @@ wget -P $SIBYLLA_HOME/downloads/ http://it.apache.contactlab.it/spark/spark-2.4.
 tar -xzvf $SIBYLLA_HOME/downloads/spark-2.4.4-bin-hadoop2.7.tgz -C $SIBYLLA_HOME/3d-party/spark-domain/spark-home
 ln -s $SIBYLLA_HOME/3d-party/spark-domain/spark-home/spark-2.4.4-bin-hadoop2.7 $SIBYLLA_HOME/3d-party/spark-domain/spark
 
+# TO UPDATE WITH SPARK-CLASS FILE HOSTED BY GV REPO
 # set spark to run with jdk8
-
-
+rm -f $SIBYLLA_HOME/3rd-party/spark-domain/spark/bin/spark-class
+wget -P $SIBYLLA_HOME/3d-party/spark-domain/spark/bin/ https://github.com/gbiagini97/spark-setup/releases/download/spark-class-1.0/spark-class 
 
 # configure spark-defaults with mongo-spark connector
 cp $SIBYLLA_HOME/3d-party/spark-domain/spark/conf/spark-defaults.conf.template $SIBYLLA_HOME/3d-party/spark-domain/spark/conf/spark-defaults.conf
