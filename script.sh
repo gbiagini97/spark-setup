b7b618b8fe13ed6403e3420dfe939659fd8043fd
gbiagini97 <gbiagini97@gmail.com>
2019-09-30 15:22:50 +0200

    updated cacerts and x64 jdk

--- script.sh
+++ script.sh
@@ -1,30 +1,36 @@
 #!/bin/sh
 
-mkdir -p $SIBYLLA_HOME/3rd-party/spark-domain/spark-jdk
-mkdir -p $SIBYLLA_HOME/3rd-party/spark-domain/spark-home
-
 #to be removed
 mkdir $SIBYLLA_HOME/downloads
 
+mkdir -p $SIBYLLA_HOME/3rd-party/spark-domain/
+cd $SIBYLLA_HOME/3rd-party/spark-domain
+
+
 #download and uncompress jdk8
-wget -P $SIBYLLA_HOME/downloads/ https://download.java.net/openjdk/jdk8u40/ri/openjdk_jre_ri-8u40-b25-linux-i586-10_feb_2015.tar.gz
-tar -xzvf $SIBYLLA_HOME/downloads/openjdk_jre_ri-8u40-b25-linux-i586-10_feb_2015.tar.gz -C $SIBYLLA_HOME/3rd-party/spark-domain/spark-jdk
-ln -s $SIBYLLA_HOME/3rd-party/spark-domain/spark-jdk/java-se-8u40-ri/ $SIBYLLA_HOME/3rd-party/spark-domain/jdk8
+wget -P $SIBYLLA_HOME/downloads/ https://download.java.net/openjdk/jdk8u40/ri/openjdk-8u40-b25-linux-x64-10_feb_2015.tar.gz
+tar -xzvf $SIBYLLA_HOME/downloads/openjdk-8u40-b25-linux-x64-10_feb_2015.tar.gz -C .
+ln -s java-se-8u40-ri jdk8
+
+#update cacerts
+rm -f jdk8/jre/lib/security/cacerts
+wget -P jdk8/jre/lib/security/ https://github.com/gbiagini97/spark-setup/releases/download/spark-class-1.0/cacerts
+
 
 #download and uncompress spark
 wget -P $SIBYLLA_HOME/downloads/ http://it.apache.contactlab.it/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
-tar -xzvf $SIBYLLA_HOME/downloads/spark-2.4.4-bin-hadoop2.7.tgz -C $SIBYLLA_HOME/3rd-party/spark-domain/spark-home
-ln -s $SIBYLLA_HOME/3rd-party/spark-domain/spark-home/spark-2.4.4-bin-hadoop2.7 $SIBYLLA_HOME/3rd-party/spark-domain/spark
+tar -xzvf $SIBYLLA_HOME/downloads/spark-2.4.4-bin-hadoop2.7.tgz -C .
+ln -s spark-2.4.4-bin-hadoop2.7 spark
 
 # TO UPDATE WITH SPARK-CLASS FILE HOSTED BY GV REPO
 # set spark to run with jdk8
-rm -f $SIBYLLA_HOME/3rd-party/spark-domain/spark/bin/spark-class
-wget -P $SIBYLLA_HOME/3rd-party/spark-domain/spark/bin/ https://github.com/gbiagini97/spark-setup/releases/download/spark-class-1.0/spark-class 
-chmod a+x $SIBYLLA_HOME/3rd-party/spark-domain/spark/bin/spark-class
+rm -f spark/bin/spark-class
+wget -P spark/bin/ https://github.com/gbiagini97/spark-setup/releases/download/spark-class-1.0/spark-class 
+chmod a+x spark/bin/spark-class
 
 # configure spark-defaults with mongo-spark connector
-cp $SIBYLLA_HOME/3rd-party/spark-domain/spark/conf/spark-defaults.conf.template $SIBYLLA_HOME/3rd-party/spark-domain/spark/conf/spark-defaults.conf
-echo "spark.jars.packages             org.mongodb.spark:mongo-spark-connector_2.11:2.4.1\nspark.mongodb.input.partitioner MongoSamplePartitioner" >>  $SIBYLLA_HOME/3rd-party/spark-domain/spark/conf/spark-defaults.conf
+cp spark/conf/spark-defaults.conf.template spark/conf/spark-defaults.conf
+echo "spark.jars.packages             org.mongodb.spark:mongo-spark-connector_2.11:2.4.1\nspark.mongodb.input.partitioner MongoSamplePartitioner" >>  spark/conf/spark-defaults.conf
 
 
 
